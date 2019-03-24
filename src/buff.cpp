350f6704bf09bcdc64f8f8e63076acd1c0622e7a
jesus-r-mendoza <jesus_r_mendoza@yahoo.com>
2019-03-23 17:27:51 -0700

    another state

--- src/buff.cpp
+++ src/buff.cpp
@@ -10,7 +10,7 @@ using namespace std;
 
 //char chars[] =  {'!','#','$','%','&','(',')','*','+',',','-','.','/','0','1','2','3','4','5','6','7','8','9',':',';','<','=','>','?','@','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','[',']','^','_','`','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
 char chars[] = {'0','1','2','3','4','5','6','7','8','9'};
-int charsSize = strlen(chars) -1;
+int charsSize = strlen(chars)-1;
 
 char buffer[BUFF_SIZE];
 
@@ -24,10 +24,10 @@ char getNextChar() {
     if ( currIndex >= charsSize ) {
         cout << "Exited Producer Thread" << endl;
         pthread_mutex_unlock(&printLock);
-        return '\\';
+        pthread_exit(0);
     }
     currIndex++;
-    cout << "Procuder grabbed: " << chars[currIndex] << endl;
+    //cout << "Procuder grabbed: " << chars[currIndex] << endl;
     pthread_mutex_unlock(&printLock);
     return chars[currIndex];
 }
@@ -41,36 +41,28 @@ void* produce(void* arg) {
             buffer[in] = next;
             in = (in + 1) % BUFF_SIZE;
             sem_post(&full);
-        } else {
-            break;
         }
     }
 }
 
-int useNextChar(int out) {
+void useNextChar(int out) {
     pthread_mutex_lock(&printLock);
     if ( currIndex >= charsSize ) {
         cout << "Exited Consumer Thread" << endl;
         pthread_mutex_unlock(&printLock);
-        //pthread_exit(0);
-        return -1;
+        pthread_exit(0);
     }
     cout << "Consumer grabbed: " << buffer[out] << endl;
     pthread_mutex_unlock(&printLock);
-    return 0;
 }
 
 void* consume(void* arg) {
     int out = 0;
     while(true) {
         sem_wait(&full);
-        int code = useNextChar(out);
-        if ( code != -1 ) {
-            out = (out + 1) % BUFF_SIZE;
-            sem_post(&empty);
-        } else {
-            break;
-        }
+        useNextChar(out);
+        out = (out + 1) % BUFF_SIZE;
+        sem_post(&empty);
     }
 }
 
